46cdb5f34b2536905ceacfb3742830e2a1b5a86f
Nadezhda Petrushina <130567482+hopenz@users.noreply.github.com>
2024-10-23 13:32:13 +0500

    feature: Написание скриптов для заполнения БД (#5)
    *feature: Написание скриптов для заполнения БД
    
    migrations.scripts:
    - Скрипт для заполнения тестовыми данными таблицы

---
+++ backend/migrations/scripts/clearning_test_data_in_the_database.sql
@@ -0,0 +1,21 @@
+/* Скрипт для очищения таблицы тариф */
+DELETE FROM tariff;
+
+/* Скрипт для очищения таблицы сервис */
+DELETE FROM service;
+
+/* Скрипт для очищения таблицы пользователей */
+DELETE FROM "user";
+
+/* Скрипт для очищения таблицы паспорта пользователей */
+DELETE FROM user_passport;
+
+/* Скрипт для очищения таблицы паспорта пользователей */
+DELETE FROM phone_number;
+
+/* Скрипт для очищения таблицы паспорта пользователей */
+DELETE FROM phone_number_service;
+
+DELETE FROM phone_number_tariff;
+
+
