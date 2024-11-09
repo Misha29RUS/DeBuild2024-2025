a535485996b8b5742f7b223740fbcf6c7bbcf84e
Nadezhda Petrushina <130567482+hopenz@users.noreply.github.com>
2024-11-09 20:57:03 +0500

    Реализация эндпойнтов для смена тарифа (#12)
    * feature: Реализация эндпойнтов для смена тарифа
    
    - Реализация PhoneNumberTariffController с эндпойнтами для смены тарифа
    - Реализация PhoneNumberTariffService с методами для связки controller с repository и реализацией бизнес логики
    - Реализация мапперов для преобразования entity классов в DTO
    - Добавление обработки ошибок в случае недостатка средств на балансе
    - Добавлние обработки ошибок при подключении гибких тарифов

---
+++ backend/billingCore/src/main/java/ru/alfa/data/dto/tariff/ResponseWithTariffPhoneNumberTariffDto.java
@@ -0,0 +1,16 @@
+package ru.alfa.data.dto.tariff;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+/**
+ * DTO for {@link ru.alfa.data.entity.tariff.PhoneNumberTariff}
+ */
+public record ResponseWithTariffPhoneNumberTariffDto(Long id, ResponseTariffDto tariff, Boolean isActive,
+                                                     LocalDate dateOfStartPeriod, LocalDate dateOfEndPeriod,
+                                                     Integer remainingMinutes, Integer remainingSms,
+                                                     Double remainingGigabytes,
+                                                     Integer countMinutesAtStartOfPeriod,
+                                                     Integer countSmsAtStartOfPeriod,
+                                                     Double countGigabytesAtStartOfPeriod) implements Serializable {
+}
