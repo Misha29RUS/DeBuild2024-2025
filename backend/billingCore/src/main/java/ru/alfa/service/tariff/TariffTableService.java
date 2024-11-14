dca82db22e29fa91e86f544497eb747fa899322b
hopenz <petrushina_03@inbox.ru>
2024-11-15 01:03:00 +0500

    Реализация эндпойнта для таблицы тарифов
    * Реализация эндпойнта для вывода отфильтрованных тарифов
    
    - Реализация TariffTableController с эндпойнтами для вывода отфильтрованных тарифов
    - Реализация TariffTableService с методами для связки controller с repository и реализацией бизнес логики по построению динамического запроса к БД
    - Реализация JPA-Specification для построения динамического запроса к БД
    - Добавила тестовые данные тарифов

---
+++ backend/billingCore/src/main/java/ru/alfa/service/tariff/TariffTableService.java
@@ -0,0 +1,39 @@
+package ru.alfa.service.tariff;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.alfa.data.dto.tariff.RequestFiltersForTariffsTableDto;
+import ru.alfa.data.dto.tariff.ResponseTariffDto;
+import ru.alfa.data.entity.tariff.Tariff;
+import ru.alfa.data.mapper.tariff.TariffMapper;
+import ru.alfa.data.repository.tariff.TariffRepository;
+
+@Service
+@RequiredArgsConstructor
+public class TariffTableService {
+
+    private final TariffRepository tariffRepository;
+
+    private final TariffMapper tariffMapper;
+
+    @Transactional
+    public Page<ResponseTariffDto> getTariffsWithFilters(
+            Integer page, Integer size,
+            RequestFiltersForTariffsTableDto requestFiltersForTariffsTableDto){
+        Pageable pageable = PageRequest.of(page,size);
+
+        Specification<Tariff> tariffSpecification = Specification.
+                where(TariffTableSpecification.hasName(requestFiltersForTariffsTableDto.name())).
+                and(TariffTableSpecification.hasStatus(requestFiltersForTariffsTableDto.status())).
+                and(TariffTableSpecification.hasType(requestFiltersForTariffsTableDto.type()));
+
+        Page<Tariff> tariffs = tariffRepository.findAll(tariffSpecification,pageable);
+
+        return tariffs.map(tariffMapper::toResponseDto);
+    }
+}
---
+++ backend/billingCore/src/main/java/ru/alfa/service/tariff/TariffTableSpecification.java
@@ -0,0 +1,30 @@
+package ru.alfa.service.tariff;
+
+import org.springframework.data.jpa.domain.Specification;
+import ru.alfa.data.entity.tariff.Tariff;
+import ru.alfa.data.entity.tariff.enums.TariffStatus;
+import ru.alfa.data.entity.tariff.enums.TariffType;
+
+public class TariffTableSpecification {
+
+    public static Specification<Tariff> hasName(String name){
+        return(root, query, criteriaBuilder) -> {
+            if (name == null) return null;
+            return criteriaBuilder.equal(root.get("name"), name);
+        };
+    }
+
+    public static Specification<Tariff> hasType(TariffType type){
+        return(root, query, criteriaBuilder) -> {
+            if (type == null) return null;
+            return criteriaBuilder.equal(root.get("type"),type);
+        };
+    }
+
+    public static Specification<Tariff> hasStatus(TariffStatus status){
+        return (root, query, criteriaBuilder) -> {
+            if (status == null) return null;
+            return criteriaBuilder.equal(root.get("status"),status);
+        };
+    }
+}
