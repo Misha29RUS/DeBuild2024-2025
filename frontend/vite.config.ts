1d41f61786c02ea879b84de01f4734c8488e3542
Misha29RUS <misha12342003@mail.ru>
2025-01-13 17:00:35 +0500

    Тест пайплайна на деплой

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,16 +3,16 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  target: "http://billing-core-api:8080",
-  plugins: [react(), svgr()]
-  // server: {
-  //   proxy: {
-  //     "/api": {
-  //       //target: "http://billing-core-api:8080", // Адрес вашего бэкенда
-  //       target: "http://62.113.105.104:8080", // Адрес вашего бэкенда
-  //       changeOrigin: true, // Изменяет заголовок Origin для обхода CORS
-  //       rewrite: (path) => path.replace(/^\/api/, "/api"),
-  //     },
-  //   },
-  // },
+  // target: "http://billing-core-api:8080",
+  plugins: [react(), svgr()],
+  server: {
+    proxy: {
+      "/api": {
+        //target: "http://billing-core-api:8080", // Адрес вашего бэкенда
+        target: "http://62.113.105.104:8080", // Адрес вашего бэкенда
+        changeOrigin: true, // Изменяет заголовок Origin для обхода CORS
+        rewrite: (path) => path.replace(/^\/api/, "/api"),
+      },
+    },
+  },
 });
